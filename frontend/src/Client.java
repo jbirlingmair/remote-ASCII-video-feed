fb5055ccbf83b1c11c9ad59af6a11cdd3aaeef85
jbirlingmair <122916897+jbirlingmair@users.noreply.github.com>
2024-12-04 16:50:35 -0700

    Add files via upload
    Solved some bugs and improved server connection functionality

--- frontend/src/Client.java
+++ frontend/src/Client.java
@@ -1,35 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.util.Random;
 
 public class Client extends Thread {
-    String user_input;
     App app;
-    
+    String address;
+    int port;
 
     public Client(String user_input, App app) {
-        this.user_input = user_input;
+        address = user_input.substring(0, user_input.lastIndexOf(':'));
+        port = Integer.parseInt(user_input.substring(user_input.lastIndexOf(':') + 1));
         this.app = app;
     }
 
-    //runs when address is entered into textbox
     @Override
     public void run() {
-        //setup serversocket
+        try {
+            /* Socket socket = new Socket(address, port);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); */
 
-        while (true) {
-            //loop this code
-            updateApp();
+            while (true) {
+
+                //EXAMPLE CODE: to show ui function
+                Random rand = new Random();
+                String test = "";
+                for (int i = 0; i < 128 * 72; i++) {
+                    test += rand.nextInt(10);
+                }
+                app.setOutput(test);
+                sleep(500);
+
+
+                // app.setOutput(reader.readLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    private void updateApp() {
-        //generate random characters
-        Random rand = new Random();
-        String test = "";
-        for (int i = 0; i < 128 * 72; i++) {
-            test += rand.nextInt(10);
-        }
-
-        //update app with string of length 128 * 72
-        app.setOutput(test);
     }
 }
